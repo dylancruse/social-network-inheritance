b96da18ba2ba5e43e99a8f1816e592ae8ce0e7fb
Dylan Cruse <dylancruse@live.com>
2018-06-12 01:21:41 +0000

    Update posts module, tests_posts passing

--- social_network/accounts.py
+++ social_network/accounts.py
@@ -1,10 +1,14 @@
 
 class User(object):
     def __init__(self, first_name, last_name, email):
-        pass
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.posts = []
 
     def add_post(self, post):
-        pass
+        post.user = self
+        self.posts.append(post)
 
     def get_timeline(self):
         pass
