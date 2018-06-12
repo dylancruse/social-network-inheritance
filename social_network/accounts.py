08c705e2c54b9606b22c5c6c17eb2750e8550921
Dylan Cruse <dylancruse@live.com>
2018-06-12 01:52:17 +0000

    Relocate main.py, update accounts module, test_accounts passing

--- social_network/accounts.py
+++ social_network/accounts.py
@@ -5,13 +5,18 @@ class User(object):
         self.last_name = last_name
         self.email = email
         self.posts = []
+        self.following = []
 
     def add_post(self, post):
         post.user = self
         self.posts.append(post)
 
     def get_timeline(self):
-        pass
+        all_posts = []
+        for user in self.following:
+            for post in user.posts:
+                all_posts.append(post)
+        return sorted(all_posts, key=lambda post: post.timestamp)
 
     def follow(self, other):
-        pass
+        self.following.append(other)
